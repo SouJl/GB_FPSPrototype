2232787e3b8107eba8b519f5dc23e0826d59fd31
Akexandr <meln.alex97@gmail.com>
2022-09-12 22:04:41 +0700

    Update Dictionary OrderBy example
    Доработка к заданию по 7 уроку

--- Assets/Scripts/Test/LyambdaTestScript.cs
+++ Assets/Scripts/Test/LyambdaTestScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -25,17 +26,18 @@ public class LyambdaTestScript : MonoBehaviour
         var expandDictOdrder = new Dictionary<string, int>(dict);
         var shortDictOdrder = new Dictionary<string, int>(dict);
        
+        int GetPairValue(KeyValuePair<string, int> pair) => pair.Value;
+        Func<KeyValuePair<string, int>, int> predicate = GetPairValue;
+
         Debug.Log("Развернутое обращение к OrderBy:");
-        foreach(var pair in expandDictOdrder.OrderBy(GetPairValue).ToArray()) 
+        foreach(var pair in expandDictOdrder.OrderBy(predicate).ToArray()) 
         {
             Debug.Log($"{pair.Key} - {pair.Value}");
         }
         Debug.Log("Свернутое обращение к OrderBy через лямбду:");
-        foreach (var pair in shortDictOdrder.OrderBy(d => d.Value).ToArray())
+        foreach (var pair in shortDictOdrder.OrderBy(pair => pair.Value).ToArray())
         {
             Debug.Log($"{pair.Key} - {pair.Value}");
         }
     }
-
-    private int GetPairValue(KeyValuePair<string, int> pair) => pair.Value;
 }
